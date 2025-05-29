7dd7baa125119d81f645ad2dfc6fdde25da0f752
João Pedro Madeira <134444797+jumamjir@users.noreply.github.com>
2025-05-29 11:20:49 -0300

    jdbc-postgres

---
+++ jdbc-postgres/src/entities/Order.java
@@ -0,0 +1,74 @@
+package entities;
+
+import java.time.Instant;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Order {
+
+    private Long id;
+    private Double latitude;
+    private Double longitude;
+    private Instant moment;
+    private OrderStatus status;
+
+    private List<Product> products = new ArrayList<>();
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Double getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(Double latitude) {
+        this.latitude = latitude;
+    }
+
+    public Double getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(Double longitude) {
+        this.longitude = longitude;
+    }
+
+    public Instant getMoment() {
+        return moment;
+    }
+
+    public void setMoment(Instant moment) {
+        this.moment = moment;
+    }
+
+    public OrderStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(OrderStatus status) {
+        this.status = status;
+    }
+
+    public List<Product> getProducts() {
+        return products;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("Order{");
+        sb.append("id=").append(id);
+        sb.append(", latitude=").append(latitude);
+        sb.append(", longitude=").append(longitude);
+        sb.append(", moment=").append(moment);
+        sb.append(", status=").append(status);
+        sb.append('}');
+        return sb.toString();
+    }
+        
+    }
